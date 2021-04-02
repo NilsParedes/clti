83c669ac6c7111b9437e16ac13f85bed9a4b9546
Nils Paredes <nils.parsa@gmail.com>
2021-04-02 10:03:23 -0500

    Add legend model

---
+++ lib/src/models/Legend.dart
@@ -0,0 +1,14 @@
+class Legend {
+  Legend(this._title, this._content);
+
+  String _title;
+  String _content;
+
+  String getTitle() {
+    return this._title;
+  }
+
+  String getContent() {
+    return this._content;
+  }
+}
